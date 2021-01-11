8ea42895b7855f82e796eb9bcefbb95b0981940b
Lucas Sivolella <lucassivolella@hotmail.com>
2021-01-11 07:30:38 -0300

    First commit

---
+++ ConsoleChest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleChest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
